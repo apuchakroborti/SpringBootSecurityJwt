6521d730862b40b80acfc5531b2a5d07b4d44a32
Apu Kumar Chakroborti <apuchakroborti50@gmail.com>
2022-12-03 02:04:48 +0600

    ehcache first commit

--- src/main/java/com/apu/example/springsecurityjwt/entity/CustomUser.java
+++ src/main/java/com/apu/example/springsecurityjwt/entity/CustomUser.java
@@ -3,6 +3,7 @@ package com.apu.example.springsecurityjwt.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 
@@ -11,6 +12,7 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class CustomUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
